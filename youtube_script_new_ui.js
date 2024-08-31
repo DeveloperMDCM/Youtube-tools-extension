c160a4d69d856ccae20027c8ddce41a59766b9c8
🅜🅞🅘🅢🅔🅢 <martinezmoises75@gmail.com>
2024-08-31 14:09:58 -0500

    feat: update @version

--- youtube_script_new_ui.js
+++ youtube_script_new_ui.js
@@ -70,7 +70,7 @@
 // @description:en Youtube Tools All in one local Download mp4, MP3 HIGT QUALITY
 // @description Youtube Tools All in one local Download mp4, MP3 HIGT QUALITY
 // @homepage     https://github.com/DeveloperMDCM/
-// @version      2.2.60
+// @version      2.2.70
 // @author       MDCM
 // @match        https://*.youtube.com/*
 // @exclude      *://music.youtube.com/*
